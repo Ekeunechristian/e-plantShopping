b128acd7a61db1a8eea5add346e9dec74f81d5fe
Ekeunechristian <mekomouekeune@gmail.com>
2024-08-02 16:59:00 -0400

    Debuging cardItem

--- src/store.js
+++ src/store.js
@@ -1,9 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
+// store.js
 
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+// Import the necessary functions and files
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice.jsx'; // Adjust the path if necessary
+
+// Configure the Redux store
+const store = configureStore({
+  reducer: {
+    cart: cartReducer, // Associate the cart reducer with the 'cart' slice of state
+  },
 });
-export default store
+
+// Export the configured store
+export default store;
